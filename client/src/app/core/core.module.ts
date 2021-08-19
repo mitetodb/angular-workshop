2340268e7403f1faa8ec5057da3e1d68a2f7d82e
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-08-19 15:12:21 +0200

    create aside and theme components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AsideComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AsideComponent
   ]
 })
 export class CoreModule { }
