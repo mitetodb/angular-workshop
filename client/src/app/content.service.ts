32424bfdd3ec8008dcfe15c6f72950c38c696378
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-08-19 19:50:05 +0200

    import environment api url

--- client/src/app/content.service.ts
+++ client/src/app/content.service.ts
@@ -2,16 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost, ITheme } from './shared/interfaces';
 
+import { environment } from '../environments/environment';
+const API_URL = environment.apiURL;
+
 @Injectable()
 export class ContentService {
+
 
   constructor(private http: HttpClient) { }
   
   loadThemes() {
-    return this.http.get<ITheme[]>('http://localhost:3000/api/themes?sortby=subsribers.length');
+    return this.http.get<ITheme[]>(API_URL + '/themes?sortby=subsribers.length');
   }
 
   loadPosts() {
-    return this.http.get<IPost[]>('http://localhost:3000/api/posts?limit=5&sortby=created_at');
+    return this.http.get<IPost[]>(API_URL + '/posts?limit=5&sortby=created_at');
   }
 }
